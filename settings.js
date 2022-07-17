4d14a8d2fe902a073b27ff6bf58923879a8df9ed
Shemuwel <92962023+kshemuwel@users.noreply.github.com>
2022-07-16 19:07:52 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -59,7 +59,7 @@ global.sendwelcome = ``
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = ``
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `බෝලේ 🥰❤️`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
